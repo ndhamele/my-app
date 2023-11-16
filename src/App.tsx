9c167b484575772b8a7bce3c96c69f973050b0f2
Neha Dhamele <ndhamele@uncc.edu>
2023-11-16 12:45:06 -0500

    canvas dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 import Box from '@mui/material/Box';
 import './styles.css';
 
